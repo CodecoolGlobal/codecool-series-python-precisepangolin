bbb4c8bbe180b4ad2a7ec96f300ef6061de9be11
Milena Kolakowska <nekoloid@gmail.com>
2023-01-19 16:40:21 +0100

    All modules

--- data/queries.py
+++ data/queries.py
@@ -1,8 +1,24 @@
 from data import data_manager
 from psycopg2 import sql
 
+
+def get_actors():
+    query = """
+    SELECT actors.name as name, actors.id as actor_id, STRING_AGG (shows.title, ', ' ORDER BY shows.title ) AS movies, 
+    JSON_OBJECT_AGG(shows.id, shows.title) as show_details
+    FROM actors
+    FULL JOIN show_characters on actors.id = show_characters.actor_id
+    FULL JOIN shows on show_characters.show_id = shows.id
+    GROUP BY actors.id, name
+    ORDER BY name ASC
+    LIMIT 20
+
+    """
+    return data_manager.execute_select(query)
+
+
 def get_shows():
-    return data_manager.execute_select('SELECT id, title FROM shows;')
+    return data_manager.execute_select("SELECT id, title, ROUND(rating::numeric,2) AS rating FROM shows;")
 
 
 def get_most_rated():
@@ -24,6 +40,16 @@ def get_shows_count():
     select count(*) as count from shows"""
     return data_manager.execute_select(query, None, False)
 
+
+def get_show_title(show_id):
+    query = """
+    SELECT title
+    FROM shows
+    WHERE id = %(show_id)s
+    """
+    return data_manager.execute_select(query, {"show_id": show_id})
+
+
 def get_shows_details(order_by, order="DESC", page_number=1, shows_per_page=15):
     query = sql.SQL("""
     SELECT shows.id AS id, title, TO_CHAR(year, 'YYYY Mon DD') AS year, runtime, TO_CHAR(rating::float,'999.9') AS rating, 
@@ -38,7 +64,9 @@ def get_shows_details(order_by, order="DESC", page_number=1, shows_per_page=15):
     LIMIT %(limit)s
     OFFSET %(offset)s
     """).format(order_by=sql.Identifier(order_by))
-    return data_manager.execute_select(query, {"order": order, "limit":shows_per_page, "offset": (page_number - 1 ) * shows_per_page})
+    return data_manager.execute_select(query, {"order": order, "limit": shows_per_page,
+                                               "offset": (page_number - 1) * shows_per_page})
+
 
 def get_show_details(show_id):
     query = """
@@ -71,6 +99,7 @@ def get_show_genres(show_id):
     return data_manager.execute_select(query,
                                        {"show_id": show_id}, False)
 
+
 def get_show_seasons(showid):
     query = """
     SELECT season_number as number, title, overview
@@ -78,3 +107,80 @@ FROM seasons
 WHERE show_id = %(showid)s
 ORDER BY season_number"""
     return data_manager.execute_select(query, {"showid": showid})
+
+
+def get_ten_movies():
+    query = """
+    SELECT title, ROUND(rating::numeric,2) AS rating
+    FROM shows
+    FULL JOIN show_characters on shows.id = show_characters.show_id
+    GROUP BY title, rating
+    ORDER BY count(actor_id) DESC
+    LIMIT 10"""
+    return data_manager.execute_select(query)
+
+
+def get_ordered_shows(order):
+    query = """
+    SELECT shows.title, ROUND(shows.rating::numeric,0) AS rating, count(DISTINCT episodes.id) AS episode_count
+    FROM shows
+    FULL JOIN seasons on shows.id = seasons.show_id
+    FULL JOIN episodes on episodes.season_id = seasons.id
+    GROUP BY shows.title, shows.rating
+    ORDER BY
+    CASE WHEN %(order)s = 'ASC' THEN count(DISTINCT episodes.id) END ASC,
+    CASE WHEN %(order)s = 'DESC' THEN count(DISTINCT episodes.id) END DESC
+    LIMIT 10"""
+    return data_manager.execute_select(query, {"order": order})
+
+
+def get_genres():
+    query = """
+    SELECT name, id
+    FROM genres
+    WHERE name != 'None'
+    ORDER BY name ASC"""
+    return data_manager.execute_select(query)
+
+
+def get_actors_by_show(show_id):
+    query = """
+    SELECT actors.name, actors.id, show_characters.character_name as character_name
+    FROM actors
+    FULL JOIN show_characters on actors.id = show_characters.actor_id
+    WHERE show_id = %(show_id)s
+    ORDER BY actors.name
+    """
+    return data_manager.execute_select(query, {"show_id": show_id})
+
+
+def get_actor_by_id(actor_id):
+    query = """
+    SELECT *
+    FROM actors
+    WHERE id = %(actor_id)s"""
+    return data_manager.execute_select(query, {"actor_id": actor_id})
+
+
+def get_filtered_actors(genre, name):
+    query = """
+    SELECT actors.name, STRING_AGG (DISTINCT genres.name, ', ' ORDER BY genres.name ) AS genres
+    FROM actors
+    FULL JOIN show_characters on actors.id = show_characters.actor_id
+    FULL JOIN show_genres on show_genres.show_id = show_characters.show_id
+    FULL JOIN genres on show_genres.genre_id = genres.id
+    WHERE genres.name LIKE %(genre)s AND actors.name ILIKE %(name)s
+    GROUP BY actors.name
+    LIMIT 20
+    """
+    return data_manager.execute_select(query, {"genre": genre, "name": '%{}%'.format(name)})
+
+
+def get_birthday_actors():
+    query = """
+    SELECT name, birthday
+    FROM actors
+    WHERE death IS NULL
+    ORDER BY birthday ASC
+    LIMIT 100"""
+    return data_manager.execute_select(query)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+import json
+
+from flask import Flask, render_template, url_for, redirect, request
 from data import queries
 import math
-import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,7 @@ app = Flask('codecool_series')
 
 SHOWS_PER_PAGE = 15
 SHOWN_PAGE_NUMBER = 5
+
 
 @app.route('/')
 def index():
@@ -21,48 +23,69 @@ def design():
     return render_template('design.html')
 
 
-@app.route('/api/shows/order-by-<order_by>')
-@app.route('/api/shows/order-by-<order_by>-<order>')
-def api_most_rated(order_by="rating", order="DESC"):
-    shows = queries.get_shows_details(order_by, order)
-    return render_template('most-rated-api.html', shows=shows, order=order, order_by=order_by)
+@app.route('/actors')
+def actors():
+    actors = queries.get_actors()
+    return render_template('actors.html', actors=actors)
 
 
-@app.route('/api/json/shows/order-by-<order_by>')
-@app.route('/api/json/shows/order-by-<order_by>-<order>')
-def api_json_most_rated(order_by="rating", order="DESC"):
-    shows = queries.get_shows_details(order_by, order)
-    return json.dumps(shows)
+@app.route('/shows/most-rated/')
+@app.route('/shows/order-by-<order_by>-<order>')
+def shows_redirect():
+    return redirect(url_for('most_rated', order_by="rating", order="DESC", page_number=1))
 
 
 
-@app.route('/shows/most-rated')
+
+@app.route('/filter-actors', methods=['GET', 'POST'])
+def filter_actors(chosen_genre="Action", name=" "):
+    genres = queries.get_genres()
+    chosen_genre = request.args.get('genre')
+    if chosen_genre is None:
+        chosen_genre = "Action"
+    print(f"Genre: '{chosen_genre}'")
+    name = request.args.get('name')
+    if name is None:
+        name = ""
+    print(f"Name: '{name}'")
+    actors = queries.get_filtered_actors(chosen_genre, name)
+
+    return render_template('filter-actors.html', actors=actors, genres=genres,
+                           chosen_genre=chosen_genre, name=name)
+
+
+@app.route('/ordered-shows')
+@app.route('/ordered-shows/<order>')
+def ordered_shows(order="DESC"):
+    movies = queries.get_ordered_shows(order)
+    for movie in movies:
+        movie['rating_stars'] = round(movie['rating']) * "*"
+    return render_template('ordered-shows.html', movies=movies, order=order)
+
+
 @app.route('/shows/most-rated/<int:page_number>')
 @app.route('/shows/order-by-<order_by>')
 @app.route('/shows/order-by-<order_by>/<int:page_number>')
-@app.route('/shows/order-by-<order_by>-<order>')
 @app.route('/shows/order-by-<order_by>-<order>/<int:page_number>')
 def most_rated(order_by="rating", order="DESC", page_number=1):
-    count = queries.get_shows_count()
-    page_count = int(count['count']) // SHOWS_PER_PAGE
-    #shown_page_start = page_number - ((SHOWN_PAGE_NUMBER - 1)//2)
-    #shown_page_end = page_number + ((SHOWN_PAGE_NUMBER - 1)//2)
-    shown_page_start = page_number - 2
-    shown_page_end = page_number + 2
-    if shown_page_start < 1:
-        shown_page_start = 1
-        shown_page_end = SHOWN_PAGE_NUMBER
-    elif shown_page_end > page_count:
-        shown_page_start = page_count - 4
-        shown_page_end = page_count - 1
-
-    #shows = queries.get_most_rated()
+    # shows = queries.get_most_rated()
     # title, year, runtime, and rating co
     if order_by not in ["title", "rating", "year", "runtime"]:
         return render_template('error.html')
 
-
     shows = queries.get_shows_details(order_by, order, page_number, SHOWS_PER_PAGE)
+
+    count = queries.get_shows_count()
+    page_count = int(count['count']) // SHOWS_PER_PAGE
+    shown_page_start = page_number - ((SHOWN_PAGE_NUMBER - 1) // 2)
+    shown_page_end = page_number + ((SHOWN_PAGE_NUMBER - 1) // 2)
+    if shown_page_start < 1:
+        shown_page_start = 1
+        shown_page_end = SHOWN_PAGE_NUMBER
+    elif shown_page_end > page_count:
+        shown_page_start = page_count - SHOWS_PER_PAGE + 1
+        shown_page_end = page_count
+
     return render_template(
         'most-rated.html',
         shows=shows,
@@ -74,11 +97,20 @@ def most_rated(order_by="rating", order="DESC", page_number=1):
         page_count=page_count)
 
 
-@app.route('/api/show/<show_id>')
-def api_get_show(show_id):
-    show_details = queries.get_show_details(show_id)
+@app.route('/api/show/<id>')
+def api_get_show(id):
+    show_details = queries.get_show_details(id)
     return json.dumps(show_details)
-    #return json
+
+
+@app.route('/shows/<int:show_id>/actors')
+def show_actors(show_id):
+    shows_actors = queries.get_actors_by_show(show_id)
+    show_details = queries.get_show_title(show_id)[0]
+    return render_template('shows_actors.html', actors=shows_actors, show=show_details)
+
+
+
 
 @app.route('/shows/<show_id>')
 def detailed_view(show_id):
@@ -90,10 +122,39 @@ def detailed_view(show_id):
     runtime_str = (str(h) + 'h ' if h else '') + (str(m) + 'm ' if m else '')
 
     show_details['runtime'] = runtime_str
-    show_details['video_id'] = show_details['trailer'][show_details['trailer'].find('=') + 1:]
+    if show_details['trailer'] is not None:
+        show_details['video_id'] = show_details['trailer'][show_details['trailer'].find('=') + 1:]
+    else:
+        show_details['video_id'] = None;
 
     return render_template('detailed-view.html', show_detail=show_details,
                            genres=genres, seasons=seasons)
+
+
+@app.route('/ratings')
+def ratings_list():
+    movies = queries.get_ten_movies()
+    shows = queries.get_shows()
+    ratings = []
+    for show in shows:
+        ratings.append(show['rating'])
+
+    avg_rating = sum(ratings) / len(ratings)
+    for movie in movies:
+        movie['avg'] = "{:.2f}".format(movie['rating'] - avg_rating)
+        if movie['rating'] - avg_rating > 0:
+            movie['avg'] = "+" + movie['avg']
+    return render_template('ratings.html', movies=movies)
+
+@app.route('/actors/<int:actor_id>')
+def actor_details(actor_id):
+    actor = queries.get_actor_by_id(actor_id)[0]
+    return render_template('actor-details.html', actor=actor)
+
+@app.route('/birthday-actors')
+def birthday_actors():
+    actors = queries.get_birthday_actors()
+    return render_template('birthday-actors.html', actors=actors)
 
 
 def main():
